f9b96d5f5e7509b129b790e793d32f8d6857d678
jdaufin <jdaufin@student.42.fr>
2022-01-22 21:25:38 +0100

    Implementing a case insensitive mode using environment variables setting

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,37 @@
 use std::fs;
 use std::error::Error;
+use std::env;
 
 #[cfg(test)]
 mod tests {
     use super::*;
     
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
         assert_eq!(
             vec!["safe, fast, productive."],
             search(query, contents)
+        );
+    }
+    
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
         );
     }
 }
@@ -23,7 +40,12 @@ pub const HELP: &str = "usage: $> minigrep <search pattern> <target file path>";
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-    for line in search(&config.query, &contents) {
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+    for line in results {
         println!("{}", line);
     }
     Ok(())
@@ -39,9 +61,22 @@ fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     matches
 }
 
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut matches = vec![];
+    let query = query.to_lowercase();
+    
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            matches.push(line);
+        }
+    }
+    matches
+}
+
 pub struct Config {
     query: String,
     filename: String,
+    case_sensitive: bool,
 }
 
 impl Config {
@@ -51,7 +86,9 @@ impl Config {
         }
         let query = args[1].clone();
         let filename = args[2].clone();
-
-        Ok(Config { query, filename })
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err(); // returns false if 
+        // env variable not set, true otherwise, whatever the stored value
+            
+        Ok(Config { query, filename, case_sensitive })
     }
 }
