1bb0759efc0babb2b9e0c36a813d324317fe3ebc
jdaufin <jdaufin@student.42.fr>
2022-01-22 11:53:20 +0100

    Refactoring error handling

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use std::env;
 use std::fs;
+use std::process;
+
+const HELP: &str = "usage: $> minigrep <search pattern> <target file path>";
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args)
+        .unwrap_or_else(|err| {
+            println!("Problem parsing arguments: {}", err);
+            process::exit(1);
+        });
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
     
@@ -19,10 +26,13 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, String> {
+        if args.len() < 3 {
+            return Err(format!("not enough arguments\n{}", HELP));
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
